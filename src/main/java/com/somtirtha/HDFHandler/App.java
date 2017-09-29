2f390f3901a0db29b30776ff307529face4510d0
Somtirtha Roy <somtirtha.roy@gmail.com>
2017-09-29 16:17:36 -0600

    Added reading code for HDF5 and working on HDF4 file reading

--- src/main/java/com/somtirtha/HDFHandler/App.java
+++ src/main/java/com/somtirtha/HDFHandler/App.java
@@ -1,16 +1,33 @@
-package com.somtirtha.HDFHandler;
-
+package com.somtirtha.hdfhandler;
 
 /**
- * Hello world!
+ * 
  *
  */
 public class App {
 
+	private ClassLoader classLoader;
+	private static String fileName;
+
+	public App() {
+		// TODO Auto-generated constructor stub
+		// get the hfd5 file
+		// fileName = getClass().getClassLoader().getResource("h5ex_d_checksum.h5").getFile();
+		
+		// get the hdf4 file
+		fileName = getClass().getClassLoader().getResource("MOD021KM.A2017224.1725.006.NRT.hdf").getFile();
+	}
 
 	public static void main(String[] args) {
-		// read HdfFiles
-//		HDFFileHandler hdfh = new HDFFileHandler();
-		HDFFileHandler.readHDF();
+		App app = new App();
+		
+		// create instance of hdf file handler
+		HDFFileHandler hdfh = new HDFFileHandler();
+		
+		// read hdf5 file
+		// hdfh.readHDF5(fileName);
+		
+		// read hdf4 file
+		hdfh.readHDF4(fileName);
 	}
 }
--- src/main/java/com/somtirtha/HDFHandler/HDFFileHandler.java
+++ src/main/java/com/somtirtha/HDFHandler/HDFFileHandler.java
@@ -1,33 +1,109 @@
-package com.somtirtha.HDFHandler;
+package com.somtirtha.hdfhandler;
 
 import hdf.hdf5lib.H5;
 import hdf.hdf5lib.HDF5Constants;
+import hdf.hdflib.HDFLibrary;
+//import hdf.hdf.H4File;
+
+import java.io.File;
+import java.io.IOException;
 
 public class HDFFileHandler {
 	
-	public static void readHDF() {
+	public void print2DArray(int[][] arr) {
+		for (int i = 0; i < arr.length; ++i) {
+			for (int j = 0; j < arr[0].length; ++j) {
+				System.out.print(arr[i][j] + ", ");
+			}
+			System.out.println("\n");
+		}
+	}
+
+	public void readHDF5(String fileName) {
 
 		try {
 			// open file and retrieve the file structure
-			// H5File h5file = new H5File("~/Downloads/h5ex_d_checksum.h5");
-			// h5file.open();
-			// HDFLibrary.Hopen("~/Downloads/h5ex_d_checksum.h5", 1);
-
-			String fname = "~/Downloads/h5ex_d_checksum.h5";
-			int file_id = 0;
+			int file_id = -1;
+			int dataset_id = -1;
 			int faplist_id;
 			int status;
-			faplist_id = H5.H5Pcreate(HDF5Constants.H5P_FILE_ACCESS);
-			status = H5.H5Pset_fapl_stdio(faplist_id);
-			file_id = H5.H5Fopen("SampleFile.h5", HDF5Constants.H5F_ACC_RDONLY, faplist_id);
 
-			// file_id = H5.H5Fopen(fname, HDF5Constants.H5F_ACC_RDWR,
-			// HDF5Constants.H5P_DEFAULT);
+			// Open the HDF5 file
+			// faplist_id = H5.H5Pcreate(HDF5Constants.H5P_FILE_ACCESS);
+			file_id = H5.H5Fopen(fileName, HDF5Constants.H5F_ACC_RDONLY, HDF5Constants.H5P_DEFAULT);
+
 			System.out.println(file_id);
+
+			/* Open an existing dataset. */
+			dataset_id = H5.H5Dopen(file_id, "/DS1", 0);
+
+			/*
+			 * read the entire dataset, into 'dset_data': memory type is 'native int' read
+			 * the entire dataspace to the entire dataspace, no transfer properties,
+			 */
+			int[][] dset_data = new int[32][64];
+			status = H5.H5Dread(dataset_id, HDF5Constants.H5T_NATIVE_INT, HDF5Constants.H5S_ALL, HDF5Constants.H5S_ALL,
+					HDF5Constants.H5P_DEFAULT, dset_data);
+
+			status = H5.H5Dclose(dataset_id);
+
+			// print data; this is to test teh data we read from 
+			print2DArray(dset_data);
+
+			// close the file using the file id
+			H5.H5Fclose(file_id);
 
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
+
+	}
+	
+	
+	/**
+	 * Read HDF4 file
+	 * WORKING ON THIS FUNCTION
+	 * @param fileName
+	 */
+
+	public void readHDF4(String fileName) {
+		try {
+			// open file and retrieve the file structure
+			int file_id = -1;
+			int dataset_id = -1;
+			int faplist_id;
+			int status;
+
+			// Open the HDF5 file
+//			faplist_id = H5.H5Pcreate(HDF5Constants.H5P_FILE_ACCESS);
+//			status = H5.H5Pset_fapl_stdio(faplist_id);
+			short n_dds = 0;
+			file_id = HDFLibrary.HDFopen(fileName, HDF5Constants.H5F_ACC_RDONLY, n_dds);
+
+			System.out.println(file_id);
+
+			/* Open an existing dataset. */
+//			dataset_id = HDFLibrary.HDDopen(file_id, "/DS1", 0);
+
+			/*
+			 * read the entire dataset, into 'dset_data': memory type is 'native int' read
+			 * the entire dataspace to the entire dataspace, no transfer properties,
+			 */
+			
+
+			// print data
+
+
+			// close the file using the file id
+			HDFLibrary.HDFclose(file_id);
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+
+	}
+
+	public static void extractHDF4Data() {
 
 	}
 }
